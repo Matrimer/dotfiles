96ba6af90d102f249baa9a5553f4fb1738cc8eab
Matrimer <mats.rimereit@mailbox.org>
2023-09-10 13:12:36 +0200

    added cachix.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,17 +4,21 @@
 # use release 23.05 branch of the GitHub repository as input, this is the most common input format
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
 #    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = {self, nixpkgs, hyprland, ...}: {
+  outputs = {self, nixpkgs, hyprland, home-manager, ...}: {
     nixosConfigurations.nixos-utpad = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        inputs.home-manager.nixosModules.home-manager # make home manager available to configuration.nix
+        #inputs.home-manager.nixosModules.home-manager # make home manager available to configuration.nix
+	home-manager.nixosModules.home-manager
 	{
 	# use system-level nixpkgs rather than the HM private ones
           home-manager.useGlobalPkgs = true;
