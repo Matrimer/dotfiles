b064e6c94b763100020eb592db6ec7c84eb71618
Matrimer <mats.rimereit@mailbox.org>
2023-09-10 13:14:46 +0200

    added cachix dir

---
+++ nixos/cachix/instantos.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://instantos.cachix.org"
+      ];
+      trusted-public-keys = [
+        "instantos.cachix.org-1:6GezJZuxIK59ydzdyoIw6Yr4H6DaCoLzqhPaDhB8v24="
+      ];
+    };
+  };
+}
